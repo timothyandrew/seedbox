0f7bdfec1db87d5f63e975d09c1fe8bdec447a25
Timothy Andrew <mail@timothyandrew.net>
2014-02-25 09:11:55 +0530

    Change the location of nginx root

--- seedbox-setup.sh
+++ seedbox-setup.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt-get -y install transmission-cli transmission-common transmission-daemon nginx
 sudo cp /tmp/seedbox/transmission-settings.json /etc/transmission-daemon/settings.json
 sudo cp /tmp/seedbox/nginx.conf /etc/nginx/nginx.conf
-sudo chmod 777 /usr/share/nginx/www 
-sudo rm /usr/share/nginx/www/index.html
+sudo chmod 777 /usr/share/nginx/html
+sudo rm /usr/share/nginx/html/index.html
 sudo /etc/init.d/transmission-daemon reload
 sudo /etc/init.d/nginx restart
